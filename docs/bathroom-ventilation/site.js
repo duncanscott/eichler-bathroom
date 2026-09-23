86aa43626e0379568ddcfef1adee6eaa59b2d827
Duncan Scott <dnscott@gmail.com>
2026-09-23 03:29:25 -0700

    Remove contractor brief print and download controls

--- docs/bathroom-ventilation/site.js
+++ docs/bathroom-ventilation/site.js
@@ -4,9 +4,3 @@ const openingToggle = document.querySelector('#show-opening');
 openingToggle.addEventListener('change', () => {
   document.querySelector('#grille-comparison').classList.toggle('show-openings', openingToggle.checked);
 });
-
-document.querySelector('#print-brief').addEventListener('click', () => {
-  document.body.classList.add('print-brief');
-  window.print();
-});
-window.addEventListener('afterprint', () => document.body.classList.remove('print-brief'));
